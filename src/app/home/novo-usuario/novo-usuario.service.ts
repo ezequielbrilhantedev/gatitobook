237e2cc9fdbc1f0ff7b71e647bff703a300fa054
Ribeiro Brilhante <e.ribeiro.brilhante@accenture.com>
2021-12-06 12:06:08 -0300

    Validações usando o Fomulário reativo

---
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { NovoUsuario } from './novo-usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  apiRoot = 'http://localhost:3000'
+
+  constructor(private httpClient: HttpClient) { }
+
+  cadastrarNovoUsuario(novoUsuario: NovoUsuario) {
+    return this.httpClient.post(`${this.apiRoot}/user/signup`, novoUsuario)
+  }
+}
