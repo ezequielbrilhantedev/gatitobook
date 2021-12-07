2d1bb500cb2c6fa0ff2dcd1aed57aff8e7b7ec49
Ezequiel Brilhante <ezequielrb7@gmail.com>
2021-12-07 17:05:18 -0300

    Tratamento de tokens na requisição e rotas de logout e login

--- src/app/autenticacao/autenticacao.service.ts
+++ src/app/autenticacao/autenticacao.service.ts
@@ -1,18 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { UsuarioService } from './usuario/usuario.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AutenticacaoService {
+  constructor(
+    private httClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
 
-  constructor(private httClient: HttpClient) { }
-
-  autenticar(usuario: string, senha: string): Observable<any> {
-    return this.httClient.post('http://localhost:3000/user/login', {
-      userName: usuario,
-      password: senha
-    })
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
+    return this.httClient
+      .post(
+        'http://localhost:3000/user/login',
+        {
+          userName: usuario,
+          password: senha,
+        },
+        {
+          observe: 'response',
+        }
+      )
+      .pipe(
+        tap((res) => {
+          const authToken = res.headers.get('x-access-token') ?? '';
+          this.usuarioService.salvarToken(authToken);
+        })
+      );
   }
 }
