e7ac8cf63577fd7003aeea9fa792da9a2173d2f8
Ezequiel Brilhante <e.ribeiro.brilhante@accenture.com>
2022-08-10 14:33:11 -0300

    create function like, comments and trash photo

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { TokenService } from '../autenticacao/token.service';
 import { Animais, Animal } from './animais';
 import { environment } from './../../environments/environment.prod';
+import { catchError, mapTo } from 'rxjs/operators';
 
 const API = environment.apiURL;
+const NOT_MODIFIED = '304';
 
 @Injectable({
   providedIn: 'root',
@@ -23,5 +25,16 @@ export class AnimaisService {
 
   excluirAnimal(id: number): Observable<Animal> {
     return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  curtir(id: number): Observable<boolean> {
+    return this.http
+      .post(`${API}/photos/${id}/like`, {}, { observe: 'response' })
+      .pipe(
+        mapTo(true),
+        catchError((error) => {
+          return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+        })
+      );
   }
 }
